984b6cc13e3ec1cf2b63c2dff93227ea849e77f8
sbadame <s.badame@gmail.com>
2010-05-16 12:04:21 -0400

    jQuery!!

--- header.php
+++ header.php
@@ -12,6 +12,17 @@
      <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
      <link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
      <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/jquery.js"></script>
+     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/jquery.kwicks-1.5.1.pack.js"></script>
+
+     <script>
+        $().ready(function() {
+            $('.kwicks').kwicks({
+                max: 205,
+                spacing: 5
+            });
+        });
+     </script>
 
      <?php wp_get_archives('type=monthly&format=link'); ?>
      <?php //comments_popup_script(); // off by default ?>
@@ -22,5 +33,11 @@
         <a href="<?php bloginfo('url'); ?>"><img id="header" width="375" height="55" alt="Sandro's Code" src="<?php bloginfo('template_directory'); ?>/images/header.gif" /></a>
         <div id="sublayer">
             <div class="t"><div class="b"><div class="l"><div class="r"><div class="bl"><div class="br"><div class="tl"><div class="tr">
-            <div id="meta"> <?php include(TEMPLATEPATH . '/searchform.php'); ?><?php wp_meta(); ?> </div>
+            <ul class="kwicks">
+                <li id="kwick1"></li>
+                <li id="kwick2"></li>
+                <li id="kwick3"></li>
+                <li id="kwick4"></li>
+            </ul>
+            <!-- <span id="meta"> <?php include(TEMPLATEPATH . '/searchform.php'); ?><?php wp_meta(); ?> </span> -->
 
