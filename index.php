00e265a244703bcb24daef06bb11eeee905b13fd
sbadame <s.badame@gmail.com>
2010-10-01 17:46:00 -0500

    Changed the header

--- index.php
+++ index.php
@@ -1,29 +1,9 @@
 <?php get_header(); ?>
 <body>
     <div id="main">
-        <a href="<?php bloginfo('url'); ?>"><img id="header" width="375" height="55" alt="Sandro's Code" src="<?php bloginfo('template_directory'); ?>/images/header.gif" /></a>
         <div id="sublayer">
             <span class="surround">
-                <div id="navlayer">
-<div class="t"><div class="b"><div class="l"><div class="r"><div class="bl"><div class="br"><div class="tl"><div class="tr">
-                        <!-- Grabbing the list of pages now -->
-                        <ul>
-                            <?php
-                                $output = wp_list_pages('echo=0&title_li_=');
-                                preg_match_all('!<a\s*href="(.*?)"\s*title="(.*?)">(.*?)</a>!', $output, $out, PREG_SET_ORDER);
-                                for ( $i = 0; $i < count($out); $i++){
-                                    $page = $out[$i];
-                                    if ( $i == count($out) - 1){
-                                        print("<li><a id='lastnavitem' href='$page[1]'>$page[3]</a></li>\n");
-                                    }else{
-                                        print("<li><a href='$page[1]'>$page[3]</a></li>\n");
-                                    }
-                                }
-                            ?>
-                        </ul>
-                    </div></div></div></div></div></div></div></div>
-                </div>
-
+                <?php include("heading.php"); ?>
                 <!-- Content -->
                 <div id="content">
                     <span class="surround">
