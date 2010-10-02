7c10fea1f87c10a8e319e83b178f9cbabf80c16c
sbadame <s.badame@gmail.com>
2010-10-02 16:07:38 -0500

    Fixed the stylings of the categories on the sidebar

--- sidebar.php
+++ sidebar.php
@@ -12,15 +12,17 @@
                                     );
                                     wp_list_bookmarks($args);
                             ?>
-                            <?php print(_('Archives')."\n"); ?>
+                            <?php print("<span class=\"category\">"._('Archives')."</span>\n"); ?>
                             <ul>
                                 <?php wp_get_archives('type=monthly&echo=1'); ?>
                             </ul>
-                            <?php>
+                            <?php
+                                    print("<span class='category'>");
                                     wp_register('',''); 
                                     print("<br>");
                                     wp_loginout(); 
                                     wp_meta(); 
+                                    print("</span>");
                             ?>
                             <br>
                         </div>
