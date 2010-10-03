c11ff66e6881cc10d7b2a7892f70eb17a30835d0
sbcoded <sbcoded@miles.asmallorange.com>
2010-10-03 01:39:23 -0400

    Added credit to wordpress at the footer

--- bottom.php
+++ bottom.php
@@ -1,9 +1,9 @@
                     </div></div></div></div></div></div></div></div>
                 </div> <!-- close content -->
                 <?php get_sidebar(); ?>
-                <div id="footer">Copyright &#169; 2010 <?php bloginfo('name'); ?> <br />This website, and all sub-projects that this website holds are all original creations by Sandro Badame unless stated otherwise.</div>
+                <div id="footer">Copyright &#169; 2010 <?php bloginfo('name'); ?> <br />This website, and all sub-projects that this website holds are all original creations by Sandro Badame unless stated otherwise.<br /> SBCoded is proudly powered by <a href="http://wordpress.org/">WordPress</a></div>
 
-            <?php wp_footer(); ?>
+            <?php wp_footer(); ?> <!-- The SyntaxHighlighter plugin needs wp_footer() -->
             </span> <!-- close sublayer decoration -->
         </div> <!-- close sublayer -->
     </div> <!-- close main -->
