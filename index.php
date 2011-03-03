354940be5b30781e306023aaf211be6bfc08b177
sbadame <s.badame@gmail.com>
2011-03-02 20:43:13 -0600

    I think that disqus works!

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
             if ($posted != $modified){ print " Last edited: $modified"; }
             edit_post_link('Edit', '&nbsp;|&nbsp;', '');?> </div>
         <div class="message"><?php the_content(); ?></div>
-        <div class="comments"> <?php comments_popup_link('No Comments Yet &#187;', '1 Comment &#187;', '% Comments &#187;'); ?> </div>
+        <div class="comments"> <a href="<?php the_permalink(); ?>#disqus_thread">Loading Comments...</a> </div>
     </div>
 <?php endwhile; ?>
 <div class="navlinks"> <?php posts_nav_link(); ?> </div>
@@ -19,4 +19,16 @@
     <div class="error"> </div>
 <?php endif; ?>
 <!--  end index -->
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'sbcoded'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function () {
+        var s = document.createElement('script'); s.async = true;
+        s.type = 'text/javascript';
+        s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    }());
+</script>
 <?php include('bottom.php'); ?>
--- single.php
+++ single.php
@@ -7,7 +7,20 @@
             <span class="date"><?php the_date(); ?> </span>
             <span class="meta"><?php edit_post_link('Edit', '&nbsp;|&nbsp;', ''); ?></span>
             <div class="message"><?php the_content(); ?></div>
-            <div class="comments-template"> <?php comments_template(); #Must be call from within 'The Loop' ?> </div>
+            <div class="comments-template"> 
+                <div id="disqus_thread"></div>
+                <script type="text/javascript">
+                    var disqus_shortname = 'sbcoded';
+                    var disqus_url = '<?php echo get_permalink(); ?>';
+                    (function() {
+                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+            </div>
         </div>
     <?php endwhile; ?>
     <div class="navlinks"> <?php posts_nav_link(); ?> </div>
